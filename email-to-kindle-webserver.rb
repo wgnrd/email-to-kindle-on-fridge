bfbb85c84e8f8b0b7ed13fe2823e07e15997e4f0
Dominik Wagner <dominik.waagna@gmail.com>
2017-03-18 19:13:09 +0100

    Changed Design to bigger text

--- email-to-kindle-webserver.rb
+++ email-to-kindle-webserver.rb
@@ -12,8 +12,10 @@ $last_email_uid = nil
 def fetch_last_email
   begin
     email = $gmail.inbox.emails.last
+    puts 'test'
   rescue # error handling when connection is lost
     begin
+      puts 'logout'
       $gmail.logout
     ensure
       $gmail = Gmail.new $gmail_login, $gmail_password
@@ -21,6 +23,9 @@ def fetch_last_email
     end
   end
   
+  puts 'emailid is '
+  puts email.uid
+
   if email.uid == $last_email_uid
     return
   end
@@ -44,14 +49,18 @@ get '/email' do
   <<STRING
 <html>
   <head>
+  <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <style>
 body {font-family: sans}
 p {padding-top: 0; paddding-bottom: 0; margin-top: 0; margin-bottom: 0;}
 </style>
 </head>
   <body>
+  <div class="jumbotron">
+    <h1 style="font-size: 1200%;">#{$subject}</h1>
+  </div>
     <p  style="font-size: 120%"><b>#{$sender}</b>, #{$received_at}</p>
-    <p style="font-size: 260%">#{$subject}</p>
     <img src="data:image/jpg;base64,#{Base64.encode64($image) if $image}" style="width:100%;">
 
     <script>
